960087854d13cbc939552f5a083c591f00c546be
Kakeru Sato <sato.kakeru3@gmail.com>
2026-05-22 22:39:08 +0900

    under dev

---
+++ go/evm/types.go
@@ -0,0 +1,12 @@
+//
+// types.go
+//
+package evm
+
+import (
+    "github.com/k4k3ru-hub/onchain/go/evm/erc20"
+)
+
+type ERC20Client = erc20.Client
+type ERC20TransferEvent = erc20.TransferEvent
+
